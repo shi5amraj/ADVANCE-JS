3b13f22ac9da94a3e8c02c94132914232503afbb
shi5amraj <shivamsinghtomar07@gmail.com>
2022-12-03 19:18:48 +0530

    creating resume using html

---
+++ DAY-005-(31-10-2022)/index.js
@@ -0,0 +1,27 @@
+a=10
+b=20
+
+if(a>b)
+{
+    console.log("a is greater than b")
+}
+if(a<b)
+{
+    console.log("b is greater than a") 
+}
+else{
+    console.log("number are equal") 
+}
+
+var num1=10
+var num2=20
+
+function multiply(num1,num2)
+{
+    return num1*num2
+}
+
+var res=multiply(num1,num2)
+var res1=multiply(10,20)
+console.log(res)
+console.log(res1)
