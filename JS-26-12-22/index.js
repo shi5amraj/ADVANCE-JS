f900ac3069bd9f19b54894ce17d7383422bf8567
shi5amraj <shivamsinghtomar07@gmail.com>
2023-01-03 17:11:40 +0530

    ok

---
+++ JS-26-12-22/index.js
@@ -0,0 +1,19 @@
+// var arr=[5,3,2,14,16,10,27]
+// arr.map((ele,idx)=>{
+//     document.write(`${ele}<br/>`)
+// }
+
+// )
+
+//  var arr2=["abc","def","ghi","jkl","mno"]
+
+//  var finalarr=arr2.map((elem)=>
+// {
+//     return elem.toUpperCase();
+// })
+ 
+//document.write(finalarr)
+
+var arrOfArr=[[1,2,3,4,5,6],["a","b","c","d","e","f"],[10,11,12,13,"abx","vcf"]]
+
+
